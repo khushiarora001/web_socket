682d67d1b875ccfc3d0b71ad88065730444d8135
Khushi <khushi.arora@nestorbird.com>
2025-01-28 10:45:00 +0530

    add url

--- lib/constant/api_constant.dart
+++ lib/constant/api_constant.dart
@@ -1,6 +1,6 @@
 const String baseUrl = "https://your-api-endpoint";
-const String sendOtpPath = "$baseUrl/send-otp";
-const String verifyOtpPath = "$baseUrl/verify-otp";
-const String eventsPath = "$baseUrl/events";
-const String createOrderPath = "$baseUrl/create-order";
+const String sendOtpPath = "/send-otp";
+const String verifyOtpPath = "/verify-otp";
+const String eventsPath = "/events";
+const String createOrderPath = "/create-order";
 const String webSocketUrl = 'ws://your-websocket-url';
